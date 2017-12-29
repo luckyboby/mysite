afc5211a8707341eecea5f488cc9daed5eafec0a
xiaoyan <1300162521@qq.com>
2017-12-29 09:16:02 +0800

    完善根目录

--- mytest/urls.py
+++ mytest/urls.py
@@ -3,4 +3,12 @@ from .import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail, name='index'),
+    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
+    path('index_01/', views.index_01, name='index_01'),
+    path('detail_01/<int:question_id>', views.detail_01, name='detail_01'),
+    path('detail_02/<int:question_id>', views.detail_02, name='detail_02'),
+    path('detail_03/', views.detail_03, name='detail_03'),
+    path('detail_04/<int:question_id>/', views.detail_04, name='detail_04'),
 ]
--- mytest/views.py
+++ mytest/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# -*- coding:utf-8 –*-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question, Choice
 
 
 def index(request):
     return HttpResponse('xiaoyan')
+
+
+def detail(request, question_id):
+    return HttpResponse('你正在看问题 s%.', question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def index_01(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
+
+
+def detail_01(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'detail.html', {'question': question})
+
+
+def detail_02(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
+
+
+def detail_03(request):
+    question = Question.objects.all()
+    return render(request, 'detail.html', {'question': question})
+
+
+def detail_04(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'detail_04.html', {'question': question})
